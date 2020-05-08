75ce59aae03dc0f16ea4d1f02a707a7b848e896e
gdarold <gadarold@gmail.com>
2020-05-07 21:30:46 -0300

    first commit

---
+++ avaliacoes/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from avaliacoes.models import Avaliacao
+
+admin.site.register(Avaliacao)
---
+++ comentarios/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from comentarios.models import Comentario
+
+admin.site.register(Comentario)
+
